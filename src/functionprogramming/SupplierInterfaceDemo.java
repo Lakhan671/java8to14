3d23d75df7803925ccf7ebcee7ac21d7a0b0732e
Lakhan Singh <lakhans671@gmail.com>
2020-09-06 16:16:11 +0530

    supplier

---
+++ src/functionprogramming/SupplierInterfaceDemo.java
@@ -0,0 +1,11 @@
+package functionprogramming;
+
+import java.util.function.Supplier;
+
+public class SupplierInterfaceDemo {
+    public static void main(String[] args) {
+        Supplier<Integer> supplier=()->(int)(Math.random()*1000);
+        System.out.println(supplier.get());
+        System.out.println(supplier.get());
+    }
+}
